02e5dfd517b528a559779375e821ac8c1560b34b
RohitPithani026 <rohitpithani13@gmail.com>
2025-05-09 10:06:28 +0530

    Week-33.1 Zapier Part 1

---
+++ Cohort-Week-33/hooks/src/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+const app = express();
+
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
+    const body = req.body;
+
+    // store in db a new trigger
+    await client.$transaction(async tx => {
+        const run = await client.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body
+            }
+        });
+
+        await client.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id
+            }
+        })
+    })
+})
+
+app.listen(3000);
