233446edc5a1d6675ee63851b1f6bcc0faaa2df2
RohitPithani026 <rohitpithani13@gmail.com>
2025-05-13 10:04:41 +0530

    Week-33.5 Zapier Part-5 (Frontend Part-2 Continue...)

--- Cohort-Week-33/frontend/app/signup/page.tsx
+++ Cohort-Week-33/frontend/app/signup/page.tsx
@@ -4,8 +4,17 @@ import { Appbar } from "@/components/Appbar";
 import { PrimaryButton } from "@/components/buttons/PrimaryButton";
 import { CheckFeature } from "@/components/CheckFeature";
 import { Input } from "@/components/input";
+import axios from "axios";
+import { useState } from "react";
+import { BACKEND_URL } from "../config";
+import { useRouter } from "next/navigation";
 
 export default function () {
+    const router = useRouter();
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return <div>
         <Appbar />
         <div className="flex justify-center">
@@ -25,17 +34,22 @@ export default function () {
                 </div>
                 <div className="flex-1 pt-6 pb-6 mt-12 px-4 border rounded">
                     <Input label={"Name"} onChange={e => {
-
+                        setName(e.target.value);
                     }} type="text" placeholder="Your name"></Input>
                     <Input onChange={e => {
-
+                        setEmail(e.target.value);
                     }} label={"Email"} type="text" placeholder="Your Email"></Input>
                     <Input onChange={e => {
-
+                        setPassword(e.target.value);
                     }} label={"Password"} type="password" placeholder="Password"></Input>
                     <div className="pt-4">
-                        <PrimaryButton onClick={() => {
-
+                        <PrimaryButton onClick={ async () => {
+                            const res = await axios.post(`${BACKEND_URL}/api/v1/user/signup`, {
+                                username: email,
+                                password,
+                                name
+                            });
+                            router.push("/login");
                         }} size="big">Get Started Free</PrimaryButton>
                     </div>
                 </div>
