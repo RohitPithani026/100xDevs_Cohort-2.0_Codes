f02d599d69a2b50a4e1c30b7c4ff98730a7d62f3
RohitPithani026 <rohitpithani13@gmail.com>
2025-05-23 09:09:24 +0530

    Week-35 Paytm Merchant App (Added user and merchant login)

--- Cohort-Week-35/backend/src/router/merchant.ts
+++ Cohort-Week-35/backend/src/router/merchant.ts
@@ -1,11 +1,60 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken";
+import { SigninSchema, SignupSchema } from "../types";
+import { JWT_MERCHANT_PASS } from "../config";
+
+const prismaClient = new PrismaClient();
 
 export const merchantRouter = Router();
 
-merchantRouter.post("/signup", (req, res) => {
+merchantRouter.post("/signup", async (req, res) => {
+    const parsedData = SignupSchema.safeParse(req.body);
 
+    if (!parsedData.success) {
+        return res.status(400).json({
+            message: "Invalid input",
+        });
+    }
+
+    try {  
+        await prismaClient.merchant.create({
+            data: {
+                username: parsedData.data.username,
+                password: parsedData.data.password,
+                name: parsedData.data.name
+            }
+        })
+        res.json({
+            message: "Signed up"
+        })
+    } catch (e) {
+        return res.status(403).json({
+            message: "Error while signing up"
+        })
+    }
 });
 
-merchantRouter.post("/signin", (req, res) => {
-    
+merchantRouter.post("/signin", async (req, res) => {
+    const body = req.body;
+    const parsedData = SigninSchema.safeParse(body);
+
+    const merchant = await prismaClient.merchant.findFirst({
+        where: { 
+            username: parsedData.data?.username,
+            password: parsedData.data?.password
+        }
+    })
+    if (!merchant) {
+        return res.status(403).json({
+            message: "Unable to log you in"
+        })
+    }
+    const token = jwt.sign({
+        id: merchant.id
+    }, JWT_MERCHANT_PASS);
+
+    return res.json({
+        token
+    })
 });
--- Cohort-Week-35/backend/src/router/user.ts
+++ Cohort-Week-35/backend/src/router/user.ts
@@ -1,15 +1,60 @@
 import { Router } from "express";
-import { SignupSchema } from "../types";
+import { PrismaClient } from "@prisma/client";
+import { SigninSchema, SignupSchema } from "../types";
+import { JWT_USER_PASS } from "../config";
+import jwt from "jsonwebtoken";
+
+const prismaClient = new PrismaClient();
 
 export const userRouter = Router();
 
-userRouter.post("/signup", (req, res) => {
-    const body = req.body;
-    const parsedData = SignupSchema.safeParse(body);
-    
+userRouter.post("/signup", async (req, res) => {
+    const parsedData = SignupSchema.safeParse(req.body);
 
+    if (!parsedData.success) {
+        return res.status(400).json({
+            message: "Invalid input",
+        });
+    }
+
+    try {  
+        await prismaClient.user.create({
+            data: {
+                username: parsedData.data.username,
+                password: parsedData.data.password,
+                name: parsedData.data.name
+            }
+        })
+        res.json({
+            message: "Signed up"
+        })
+    } catch (e) {
+        return res.status(403).json({
+            message: "Error while signing up"
+        })
+    }
 });
 
-userRouter.post("/signin", (req, res) => {
+userRouter.post("/signin", async (req, res) => {
+        const body = req.body;
+        const parsedData = SigninSchema.safeParse(body);
     
+        const user = await prismaClient.user.findFirst({
+            where: { 
+                username: parsedData.data?.username,
+                password: parsedData.data?.password
+            }
+        })
+        if (!user) {
+            return res.status(403).json({
+                message: "Unable to log you in"
+            })
+        }
+        const token = jwt.sign({
+            id: user.id
+        }, JWT_USER_PASS);
+    
+        return res.json({
+            token
+        })
 });
